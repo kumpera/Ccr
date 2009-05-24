6dc7b8118caadb8cc011fce35340849d495ce94f
Rodrigo Kumpera <kumpera@gmail.com>
2009-05-23 21:37:18 -0300

    Implement Arbiter.MultipleItemReceive.

--- src/Microsoft.Ccr.Core/Arbiter.cs
+++ src/Microsoft.Ccr.Core/Arbiter.cs
@@ -26,6 +26,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 using Microsoft.Ccr.Core.Arbiters;
@@ -120,6 +121,24 @@ namespace Microsoft.Ccr.Core {
 		public static MultipleItemReceiver MultipleItemReceive<T> (VariableArgumentHandler<T> handler, params Port<T>[] ports)
 		{
 			return new MultipleItemReceiver (new VariableArgumentTask<T> (ports.Length, handler), ports);
+		}
+
+		public static MultipleItemGather MultipleItemReceive<T0, T1> (PortSet<T0, T1> portSet, int totalItemCount, Handler<ICollection<T0>, ICollection<T1>> handler)
+		{
+			//public MultipleItemGather (Type[] types, IPortReceive[] ports, int itemCount, Handler<ICollection[]> handler)
+			Type[] types = new Type [] { typeof (T0), typeof (T1) };
+			IPortReceive[] ports = new IPortReceive[] { portSet.P0, portSet.P1 };
+			Handler<ICollection[]> inner_handler = (col) => {
+				//LAMEIMPL MS impl of MultipleItemGather passes an array of List<object>.
+				List<T0> l0 = new List<T0> (col [0].Count);
+				List<T1> l1 = new List<T1> (col [1].Count);
+				foreach (var o in col [0])
+					l0.Add ((T0)o);
+				foreach (var o in col [1])
+					l1.Add ((T1)o);
+				handler (l0, l1);
+			}; 
+			return new MultipleItemGather (types, ports, totalItemCount, inner_handler);
 		}
 	}
 }
